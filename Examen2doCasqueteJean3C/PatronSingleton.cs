bc5d3ab0c14432648804273028386cb1688f82fb
JeanCasquete <YinRodriguez71@gmail.com>
2022-01-20 22:04:46 -0500

    se sube el proyecto

---
+++ Examen2doCasqueteJean3C/PatronSingleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Nombre: Casquete Rodriguez Jean Pier 3C
+// Aqui uso el patron de diseño Singleton para mostrar los datos de un restaurante en los folletos del menu
+namespace ConsoleApp1
+{
+    public class PatronSingleton
+    {
+        //Privatisamos la clase Singleton con su atributo instance
+        private static PatronSingleton instance = null;
+
+        //Creamos mensaje para mostrar un mensaje en pantalla
+        //en esta caso los datos de la plantilla
+        //Por el momento este no contiene nada
+  
+        public string mensajito = "";
+
+        //Creamos el constructor PatronSingleton
+        protected PatronSingleton()  
+        {
+            mensajito = "     Restaurente Delicias del marisco"; //Aqui es donde va a mostrar el primer mensaje que se encuentra en PROGRAM.CS
+        }
+
+        public static PatronSingleton Instance //El beneficio de que utilicemos la propiedad INSTANCE de SINGLETON es que siempre será la misma
+        {                                // y por ende va ser el mismo para todo, pero aún así no es recomendable utilizarlo para todo.
+            get //Utilizamos el get para devolver a la clase instanciada 
+            {
+                if (instance == null)
+                    instance = new PatronSingleton();
+
+                return instance; //Aqui devolvemos lo que contiene la propiedad INSTANCE
+            }
+        }
+    }
+}
---
+++ Examen2doCasqueteJean3C/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //En el primero ira el mensaje que mostrara el nombre del restaurante,
+            //Utilizando asi la la propiedad Instance, el constructor y la variable
+            Console.WriteLine(PatronSingleton.Instance.mensajito);
+
+            // Aqui hacemos uso de la propiedad INSTANCE para mostrar un segundo mensaje y como podemos ver usamos la misma propiedad
+            PatronSingleton.Instance.mensajito = "Nos encuentra en la calle 504 avenida 503";
+            Console.WriteLine(PatronSingleton.Instance.mensajito);
+
+            // Aquí mostramos el 3er mensaje utilizando como los anterior usaremos la propiedad INSTANCE
+            PatronSingleton.Instance.mensajito = "        Atendemos de 8AM A 18PM   ";
+            Console.WriteLine(PatronSingleton.Instance.mensajito);
+
+            // Aquí mostramos el 4to mensaje utilizando como los anteriores usaremos la propiedad INSTANCE 
+            PatronSingleton.Instance.mensajito = "    ¡VISITENOS!   ";
+            Console.WriteLine(PatronSingleton.Instance.mensajito);
+        }
+    }
+}
